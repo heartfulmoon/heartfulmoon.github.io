320e212915f218a14fd5ec7ad2019c0f47f77af2
higebobo <higebobo@gmail.com>
2020-07-01 14:42:46 +0900

    modified

---
+++ content/post/2020/06/dreamgirls.md
@@ -0,0 +1,28 @@
+---
+title: "ドリームガールズ"
+slug: "dreamgirls"
+date: "2020-06-30T22:37:23+09:00"
+categories:
+  - 映画
+tags:
+  - 音楽
+link_url: https://www.amazon.co.jp/gp/video/detail/B00FYLZZ8C/
+thumbnailImage: https://images-na.ssl-images-amazon.com/images/I/810PRZkrHLL._SX600_.jpg
+thumbnailImagePosition: left
+#draft: true
+---
+ジャケット見るからにして Diana Ross & the Supremes だがやはり自伝を元にしているらしい。
+<!--more-->
+Diana Rossって超絶人気だったらしいけどなんかギョロ目で自分は怖い顔の印象がある。
+
+肝心の映画は古き良き時代のR&Bという感じで楽しい。
+
+{< amazon-prime >}
+
+さて、今月は勝手に映画月間と名乗って1日1本(多いときはシリーズものも含む)を頑張りなんとか達成できた。
+
+仕事では少しつらい思いというか立場に置かれつつあるけれども、映画を見ることで忘れたり、励まされたりすることもあった。
+
+ただ本数を観ることを重点に置くと肝心の内容が頭に入ってきにくくなったのも事実だ。
+
+だから来月からは少しペースを落としてじっくり観たい。
---
+++ scripts/genpost.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+# -*- mode: python -*- -*- coding: utf-8 -*-
+import argparse
+import datetime
+import os
+
+ROOT_DIR = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
+CONTENT_DIR = 'content'
+
+def check_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-s', '--slug', help='post slug')
+    parser.add_argument('-d', '--date', help='post date')
+    parser.add_argument('-t', '--type', help='content type(default post)', default='post')
+
+    return parser.parse_args()
+
+def main():
+    args = check_args()
+    if not args.slug:
+        print ('post slug is required')
+        return
+    now = datetime.datetime.now()
+    if args.date:
+        year = args.date[:4]
+        month = args.date[4:6]
+    else:
+        year = now.strftime('%Y')
+        month = now.strftime('%m')
+
+    content_dir = os.path.join(ROOT_DIR, CONTENT_DIR, args.type, year, month)
+    filepath = os.path.join(content_dir, f'{args.slug}.md')
+    if os.path.exists(filepath):
+        print (f'{filepath} is exists')
+        return
+    os.makedirs(content_dir, exist_ok=True)
+
+    output = f'''\
+---
+title: "{args.slug}"
+slug: "{args.slug}"
+date: "{now.strftime('%Y-%m-%dT%H:%M:%S+09:00')}"
+categories:
+  - 映画
+tags:
+  - ドラマ
+#link_url:
+#thumbnailImage:
+thumbnailImagePosition: left
+#draft: true
+---
+
+<!--more-->
+
+
+{{< amazon-prime >}}
+'''
+    with open(filepath, 'w') as f:
+        f.write(output)
+
+    print (f'create {filepath}')
+
+if __name__ == "__main__":
+    main()
+##### genpost.py ends here
